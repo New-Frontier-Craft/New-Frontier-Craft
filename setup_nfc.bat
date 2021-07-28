e66e3a6948f07657f6b10010d1d116ee5fe8f602
LO6AN <loganfeulner@gmail.com>
2021-07-28 17:39:51 -0500

    v3.4.1_01 Update

--- setup_nfc.bat
+++ setup_nfc.bat
@@ -1,10 +1,21 @@
 @echo off
 runtime\bin\python\python_mcp runtime\decompile.py %*
+
+:: Source Code Setup
 runtime\bin\7za a -m0=Copy src.7z src\
-runtime\bin\xdelta3 -d -s "src.7z" "NFC v3.4.0 SRC Patch.xdelta" "src_nfc.7z"
+runtime\bin\xdelta3 -d -s "src.7z" "jars\NFC v3.4.1_01 SRC Patch.xdelta" "src_nfc.7z"
 ren src src_old
 runtime\bin\7za x src_nfc.7z src
-runtime\bin\xdelta3 -d -f -s "jars\bin\minecraft.jar" "NFC v3.4.0 JAR Patch.xdelta" "jars\bin\minecraft_new.jar"
-ren jars\bin\minecraft.jar minecraft_old.jar
-ren jars\bin\minecraft_new.jar minecraft.jar
+
+::Jar Setup
+cd jars\bin\
+xcopy /v minecraft.jar minecraft_old.jar
+..\..\runtime\bin\7za a -tzip minecraft.jar .\jarfiles\*
+..\..\runtime\bin\7za d minecraft.jar -r META-INF\
+cd ..\..\
+
+::Cleanup
+del src.7z
+del src_nfc.7z
+
 pause
