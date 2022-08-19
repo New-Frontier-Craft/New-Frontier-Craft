bad621b44c14ec6764e464edef72b1ee580c1ef9
Logan <loganfeulner@gmail.com>
2022-08-19 17:03:52 -0500

    Add updatemd5 script to setup_NFC script

--- setup_nfc.bat
+++ setup_nfc.bat
@@ -6,6 +6,7 @@ runtime\bin\7za a -m0=Copy src.7z src\
 runtime\bin\xdelta3 -d -s "src.7z" "jars\NFC v3.4.2 SRC Patch.xdelta" "src_nfc.7z"
 ren src src_old
 runtime\bin\7za x src_nfc.7z src
+runtime\bin\python\python_mcp runtime\updatemd5.py %*
 
 ::Jar Setup
 cd jars\bin\
